24d1af3718bf46d9474fe0a39fce715a2c9092fd
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2025-12-31 19:17:39 +0600

    Add files via upload

---
+++ Sorting and Searching/Sum_of_Four_Values.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+// #define int long long
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) (x).begin(), (x).end()
+#define input(arr) for(auto &it:arr) cin>>it
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(arr) for(auto &it: arr) cerr<<it<<" "; cerr<<nl;
+// #define cerr if(false)cerr
+ 
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int n, k, x; cin >> n>>k;
+ 
+    
+    vector<pair<int,int>>arr;
+    for (int i = 1; i <= n; i++)
+    {
+        cin>> x; arr.push_back({x,i});
+    }
+    sort(all(arr));
+    
+    vector< pair< int,pair<int,int> > > sum;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            int at1 = arr[i].second, at2= arr[j].second;
+
+            sum.push_back({arr[i].first +arr[j].first, {min(at1,at2),max(at1,at2)}});
+        }
+    }
+
+    sort(all(sum));
+ 
+    bool f = false;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            int gap = k-arr[i].first-arr[j].first ;
+
+            int at= max(arr[i].second, arr[j].second);
+
+            auto it = lower_bound(all(sum), make_pair(gap, make_pair(at+1,at+1)));
+ 
+            if(it!=sum.end() and it->first == gap)
+            {
+                cout<< arr[i].second<<" "<<arr[j].second<<" "<<it->second.first<<" "<<it->second.second <<nl;
+                f = true;
+                break;
+            }
+        }
+        if(f)break;
+    }
+    
+    if(!f) cout<<"IMPOSSIBLE"<<nl;
+    
+}
