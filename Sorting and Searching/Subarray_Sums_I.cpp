f3bb0109272b8f9df9981634e889a87bb81595f2
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2026-01-02 13:16:11 +0600

    Add files via upload

---
+++ Sorting and Searching/Subarray_Sums_I.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define int long long
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) (x).begin(), (x).end()
+#define input(arr) for(auto &it:arr) cin>>it
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(arr) for(auto &it: arr) cerr<<it<<" "; cerr<<nl;
+// #define cerr if(false)cerr
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int n, k; cin >> n>>k;
+
+    vector<int> arr(n); input(arr);
+
+    int cnt = 0, l = 0, sum= arr.front();
+    if(arr.front()==k)cnt++;
+
+    for (int i = 1; i < n; i++)
+    {
+        sum+= arr[i];
+
+        while(sum>k) 
+        {
+            sum-= arr[l];
+            l++;
+        }
+
+        if(sum==k) cnt++;
+    }
+
+    cout<<cnt<<nl;
+}
