9f0969528fef2ece08f12be64a643ae5ecff959c
Abdullah Al Fahim <138691546+Abdullah-Al-Faheem@users.noreply.github.com>
2026-09-10 05:27:21 +0600

    Refactor
    Refactor the Sum of Divisors algorithm to improve readability and efficiency. Introduce modular exponentiation and simplify the modular inverse calculation.

--- Mathematics/Sum_of_Divisors.cpp
+++ Mathematics/Sum_of_Divisors.cpp
@@ -1,88 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define nl "\n"
-#define ll long long
-#define pi acos(-1.0)
 #define int long long
-#define NO cout<<"NO\n"
-#define YES cout<<"YES\n"
-#define sqrt(x) sqrtl(x)
-#define double long double
-#define rall(x) x.rbegin(), x.rend()
+#define rall(x) (x).rbegin(), (x).rend()
 #define all(x) (x).begin(), (x).end()
-#define fix(n) fixed<<setprecision(n) 
-#define mem(a,b) memset(a,b,sizeof(a))
-#define dbug(x) cout << #x<<" -> "<<x << nl
 #define input(arr) for(auto &it:arr) cin>>it
-#define output(arr) for(auto &it: arr) cout<<it<<" "; cout<<nl
-#define vec2d(name,n,m,val) vector<vector<int>>(name)((n),vector<int>((m),(val)))
-#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(arr) for(auto &it: arr) cerr<<it<<" "; cerr<<nl;
+// #define cerr if(false)cerr
 
 const int mod = 1e9+7;
 
-int gcdExtended(int a, int b, int* x, int* y)
+int power(int a, int b) 
 {
-    // Base Case
-    if (a == 0) 
-    {
-        *x = 0, *y = 1;
-        return b;
-    }
-    // To store results of recursive call
-    int x1, y1;
-    int gcd = gcdExtended(b % a, a, &x1, &y1);
-
-    // Update x and y using results of recursive call
-    *x = y1 - (b / a) * x1;
-    *y = x1;
-
-    return gcd;
+    if (b==0) return 1;
+    if (b&1) return a *power(a, b-1) % mod;
+    return power(a*a %mod , b/2);
 }
 
-// when M and A are coprime or gcd(A, M)==1:
-int modInverse(int A, int M) 
-{
-    int x, y;
-    int g = gcdExtended(A, M, &x, &y);
-    if (g != 1) {cout << "Inverse doesn't exist\n"; return -1;}
-    else 
-    {
-        // m is added to handle negative x
-        int res = (x % M + M) % M;
-        return res;
-    }
-}
-
-
+int modInverse(int A) {return power(A, mod - 2);}
 
 int32_t main()
 {
-    fast_io;
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
     int n; cin >> n;
 
     int ans = 0;
 
-    for (int i = 1; i <= n; )
+    for (int i = 1; i <= n;)
     {
-        int cnt = n/i;
+        int cnt = n / i;
 
         int l = i;
-        int r = n/cnt;
+        int r = n / cnt;
 
-        int sz = (r-l+1)%mod;
-        int sum = (l%mod + r%mod)%mod;
+        int sz = (r - l + 1) % mod;
+        int sum = (l % mod + r % mod) % mod;
 
-        int tmp = ((sum * sz)%mod * modInverse(2,mod))%mod;
+        int tmp = ((sum * sz) % mod * modInverse(2)) % mod;
 
+        ans = (ans + (tmp * cnt % mod) % mod) % mod;
 
-        ans= (ans + (tmp * cnt%mod)%mod ) %mod;
-
-        i = r+1;
-
+        i = r + 1;
     }
 
-    // cout << tmp<<nl;
-
-    cout << ans<<nl;
+    cout << ans << nl;
 }
