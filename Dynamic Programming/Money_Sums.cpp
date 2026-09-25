53d0686974142d62aaa907b7d51886a653c372ed
Abdullah-Al-Faheem <abdullahalfaheem9@gmail.com>
2026-09-25 19:26:14 +0600

    added sol for money-sum

---
+++ Dynamic Programming/Money_Sums.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define int long long
+#define rall(x) (x).rbegin(), (x).rend()
+#define all(x) (x).begin(), (x).end()
+#define input(arr) for(auto &it:arr) cin>>it
+#define dbug(x) cerr << (#x) << " is " << (x) << nl;
+#define output(arr) for(auto &it: arr) cerr<<it<<" "; cerr<<nl;
+// #define cerr if(false)cerr
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+
+    int n; cin >> n;
+    vector<int> arr(n+3);
+    for (int i = 1; i <= n; i++) cin>> arr[i];
+
+    int mx = 1e5;
+
+    bool dp[mx+3];
+    memset(dp,false, sizeof dp);
+    dp[0]=true;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = mx; j>= arr[i]; j--)
+        {
+            if(dp[j-arr[i]])dp[j]=true;
+        }
+    }
+
+    int cnt =0;
+    for (int i = 1; i <= mx; i++) if(dp[i])cnt++;
+
+    cout<<cnt<<nl;
+    for (int i = 1; i <= mx; i++) if(dp[i])cout<<i<<" ";
+    cout<<nl;
+
+}
